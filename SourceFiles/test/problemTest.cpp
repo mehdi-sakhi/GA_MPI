5f4afca131efc1c103e5e3b3c633c2763e14fffb
sakhi <mehdi.sakhi@uha.fr>
2017-01-05 04:13:54 +0000

    Constructor of the problem class unit testing

--- SourceFiles/test/problemTest.cpp
+++ SourceFiles/test/problemTest.cpp
@@ -1,1 +1,13 @@
 #include "catch.hpp"
+#include "problem.h"
+void problem_values_are_good(int index, int lowerLimit, int upperLimit,const Problem& problem)
+{
+	REQUIRE(problem.getIndex() == index);
+	REQUIRE(problem.getLower() == lowerLimit);
+	REQUIRE(problem.getUpper() == upperLimit);
+}
+TEST_CASE("constructor is good","[tag]"){
+	Problem p{ 1,2,3 };
+	problem_values_are_good(1, 2, 3, p);
+
+}
