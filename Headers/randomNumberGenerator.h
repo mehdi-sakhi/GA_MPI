093d3b56d8ef494993107af25e89ea6de3fe90a1
sakhi <mehdi.sakhi@uha.fr>
2017-01-05 05:56:20 +0000

    Random Numbers generation ...

---
+++ Headers/randomNumberGenerator.h
@@ -0,0 +1,11 @@
+#ifndef RANDOMNUMBERGENERATOR_H
+#define RANDOMNUMBERGENERATOR_H
+class  RandomNumberGenerator {
+
+private:
+	
+public:
+	virtual double generateNumber() const = 0;
+
+};
+#endif
---
+++ SourceFiles/GARandomNumberGenerator.cpp
@@ -0,0 +1,6 @@
+#include "GARandomNumberGenerator.h"
+
+double GARandomNumberGenerator::generateNumber() const
+{
+	return 0.0;
+}
